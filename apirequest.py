3f7e6129e748b354af7ce165df37b1a7dc8f7081
Alex Geeson <alex@alexs-mbp.home>
2021-09-24 13:49:23 +0100

    added config and gitignore

--- apirequest.py
+++ apirequest.py
@@ -1,7 +1,8 @@
 from binance.client import Client
+import config
 
-api_key = "api_key"
-secret_key = "secret_key"
+api_key = config.binance["api_key"]
+secret_key = config.binance["secret_key"]
 
 client = Client(api_key, secret_key, {"verify": False, "timeout": 20})
 # print(client.get_all_orders(symbol='BNBBTC', requests_params={'timeout': 5}))
@@ -9,14 +10,18 @@ client = Client(api_key, secret_key, {"verify": False, "timeout": 20})
 # print(client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"))
 
 
-avg_price = client.get_avg_price(symbol='BNBBTC')
+avg_price = client.get_avg_price(symbol="BNBBTC")
 # print(avg_price)
 
 
 def get_market_data_days():
-    print(client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1DAY, "2021-08-07"))
+    print(
+        client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1DAY, "2021-08-07")
+    )
+
 
 def calculate_moving_average(data=list):
-    return sum(data)/len(data)
-    
+    return sum(data) / len(data)
+
+
 get_market_data_days()
